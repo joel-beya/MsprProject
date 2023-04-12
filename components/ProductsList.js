3de49df43d71b0fd1c59f53757104e5967fb9e2b
joel-beya <122974921+joel-beya@users.noreply.github.com>
2023-04-12 16:32:03 +0200

    Coding the function for creating the table for showing products list

--- components/ProductsList.js
+++ components/ProductsList.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import { Text, View, AppRegistry } from 'react-native';
 
+//Function For creating a single product row
+function ProductRow({ product }) {
+  return <tr>
+    <td>{product.name}</td>
+    <td>{product.price}</td>
+  </tr>
+}
+//Function for creating products table
+function ProductsTable({ products }) {
+  const row =[]
+ 
+  return <table className='table'> 
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+}
+  
 class ProductsList extends Component {
   render() {
     const { products } = this.props;
-    return <View><Text>{JSON.stringify(products)}</Text></View>;
+    return <ProductsTable products={products} />
   }
 }
 export default ProductsList;
