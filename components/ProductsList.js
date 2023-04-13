4ef4cbb9ddfafc36fc5e65eac978bc666639e19a
joel-beya <122974921+joel-beya@users.noreply.github.com>
2023-04-13 12:26:49 +0200

    Working on showing products list on the phone

--- components/ProductsList.js
+++ components/ProductsList.js
@@ -1,41 +1,73 @@
 import React, { Component } from 'react';
-import { Text, View, AppRegistry } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 //Function For creating a single product row
 function ProductRow({ product }) {
-  return <tr>
-    <td>{product.name}</td>
-    <td>{product.price}</td>
-  </tr>
+  return (
+    <View style={styles.row}>
+      <Text style={styles.cell}>{product.name}</Text>
+      <Text style={styles.cell}>{product.price}</Text>
+    </View>
+  );
 }
+
 //Function for creating products table
 function ProductsTable({ products }) {
-  const row =[]
-  products.forEach((product) => {
-    row.push(<ProductRow product={product} key={product.id} />)
+  const rows = products.map((product) => {
+    return <ProductRow product={product} key={product.id} />;
   });
-  return <table className='table'> 
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Price</th>
-      </tr>
-    </thead>
-    <tbody>
-      {row}
-    </tbody>
-  </table>
+
+  return (
+    <View style={styles.table}>
+      <View style={styles.row}>
+        <Text style={styles.headerCell}>Name</Text>
+        <Text style={styles.headerCell}>Price</Text>
+      </View>
+      {rows}
+    </View>
+  );
 }
-  
+
 class ProductsList extends Component {
   render() {
     const { products } = this.props;
     return (
-        <View>
-           <Text><h1>Products List</h1></Text>
-           <ProductsTable products={products} />
-        </View>
-)
+      <View>
+        <Text style={styles.title}>Products List</Text>
+        <ProductsTable products={products} />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  table: {
+    borderWidth: 1,
+    borderColor: 'black',
+    marginVertical: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    paddingVertical: 5,
+  },
+  headerCell: {
+    fontWeight: 'bold',
+    flex: 1,
+    textAlign: 'center',
+  },
+  cell: {
+    flex: 1,
+    textAlign: 'center',
+  },
+});
+
 export default ProductsList;
