2828e95f193808d49282fb39519abf4710d22ad5
joel-beya <122974921+joel-beya@users.noreply.github.com>
2023-04-23 19:20:31 +0200

    Token

--- App.js
+++ App.js
@@ -2,46 +2,52 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ProductsList from './components/ProductsList';
 import ProductDetails from './components/ProductDetails';
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import QRCodeScanner from './components/QRCodeScanner';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createStackNavigator();
 
 function App() {
-  const [token, setToken] = useState('a');
+const [token, setToken] = useState(null);
+
+  const getToken = async () => {
+    try {
+      const response = await fetch('https://mspr4.gwendal.online/login', {
+        method: 'POST',
+        body: JSON.stringify({ email: 'retailer@mspr4.com', password: 'password' }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const data = await response.json();
+      if (data.token) {
+        console.log('Token retrieved successfully!');
+        await AsyncStorage.setItem('token', data.token);
+        setToken(data.token);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const getToken = async () => {
-      try {
-        const storedToken = await AsyncStorage.getItem('token');
-        if (storedToken !== null) {
-          setToken(storedToken);
-          console.log('Token loaded successfully!');
-        } else {
-          console.log('Token not found!');
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
     getToken();
   }, []);
 
-  //Si le token est présent dans le storage, on affiche la liste des produits
-  //Sinon, on affiche le texte Scanner qrcode
-  if(token == 'a')
-    return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Products List" component={ProductsList} />
-          <Stack.Screen name="Product Details" component={ProductDetails} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  else
-    return (
-      <Text>Scanner qrcode</Text>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {token ? (
+          <>
+            <Stack.Screen name="Products List" component={ProductsList} />
+            <Stack.Screen name="Product Details" component={ProductDetails} />
+          </>
+        ) : (
+          <Stack.Screen name="QR Code Scanner" component={QRCodeScanner} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 export default App;
