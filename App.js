cb8ae760a4f6cd12133498a92b07eeaeabb37830
joel-beya <122974921+joel-beya@users.noreply.github.com>
2023-05-03 19:46:59 +0200

    App.js updated

--- App.js
+++ App.js
@@ -2,39 +2,30 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ProductsList from './components/ProductsList';
 import ProductDetails from './components/ProductDetails';
-import React, { useState, useEffect } from 'react';
 import QRCodeScanner from './components/QRCodeScanner';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import React, { useState, useEffect } from 'react';
 
 const Stack = createStackNavigator();
 
 function App() {
-const [token, setToken] = useState(null);
-
-  const getToken = async () => {
+  const [token, setToken] = useState(null);
+  async function getToken() {
     try {
-      const response = await fetch('https://mspr4.gwendal.online/login', {
-        method: 'POST',
-        body: JSON.stringify({ email: 'retailer@mspr4.com', password: 'password' }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      const data = await response.json();
-      if (data.token) {
-        console.log('Token retrieved successfully!');
-        await AsyncStorage.setItem('token', data.token);
-        setToken(data.token);
+      const token = await AsyncStorage.getItem('token'); // récupérer le token depuis le cache
+      if (token !== null) {
+        setToken(token);
       }
     } catch (error) {
       console.log(error);
+      Alert.alert('Error', 'Failed to retrieve token. Please try again.');
     }
-  };
-
+  }
+  
   useEffect(() => {
     getToken();
   }, []);
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -44,7 +35,13 @@ const [token, setToken] = useState(null);
             <Stack.Screen name="Product Details" component={ProductDetails} />
           </>
         ) : (
-          <Stack.Screen name="QR Code Scanner" component={QRCodeScanner} />
+          <> 
+          <Stack.Screen name="QR Code Scanner" >
+          {props => <QRCodeScanner {...props} setToken={setToken} />}
+          </Stack.Screen>
+          <Stack.Screen name="Products List" component={ProductsList} />
+          <Stack.Screen name="Product Details" component={ProductDetails} />
+          </>
         )}
       </Stack.Navigator>
     </NavigationContainer>
